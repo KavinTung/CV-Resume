4abe915c4e843c5cbb0ed8091f4c73df07b45f2d
KavinTung <54610015+KavinTung@users.noreply.github.com>
2026-07-04 15:50:16 +0700

    Revise community message in prefooter section
    Updated community acknowledgment text in prefooter.

--- apps/web/src/routes/_home/-sections/prefooter.tsx
+++ apps/web/src/routes/_home/-sections/prefooter.tsx
@@ -27,10 +27,8 @@ export function Prefooter() {
 
 					<p className="text-muted-foreground leading-relaxed">
 						<Trans>
-							Reactive Resume continues to grow thanks to its vibrant community. This project owes its progress to
-							numerous individuals who've dedicated their time and skills to make it better. We celebrate the coders
-							who've enhanced its features on GitHub, the linguists whose translations on Crowdin have made it
-							accessible to a broader audience, and the people who've donated to support its continued development.
+							CV Resume - Bachhoavanphong.com continues to grow thanks to its vibrant community. This project owes its progress to
+							numerous individuals who've dedicated their time and skills to make it better.
 						</Trans>
 					</p>
 				</m.div>
